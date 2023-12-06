e26bf875cfb36cb67c59b893b5ac22db974ea056
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-12-06 14:42:51 -0800

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 i!/usr/bin/python3
 if __name__ = "__main__":
-from calculator_1.py import  add, sub, mul, div
+from calculator_1 import  add, sub, mul, div
     import sys
+
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -8,6 +7,5 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-
     else:
-        return(sub(a, b))
+        return (sub(a, b))
