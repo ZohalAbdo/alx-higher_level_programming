cced3b9bf04afb536b829a4007fed2b7c0eecb39
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2024-04-27 03:31:18 +0400

    h

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,5 +16,5 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        else
+        else:
             self.__size = size
