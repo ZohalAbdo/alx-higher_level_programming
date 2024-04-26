27fc3939bb01a3aabf1868dea254fbd3075d51a6
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2024-04-27 03:08:22 +0400

    h

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""_summary_ """
+
+
+class Square:
+    """_summary_"""
+
+    def __init__(self, size):
+        """
+
+        Args:
+            size (_type_): _description_
+        """
+
+        self.__size = size
+        
