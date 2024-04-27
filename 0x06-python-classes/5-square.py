693c8bb2eaeafb48304e485278a96ae7e37d2ad6
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2024-04-27 04:39:09 +0400

    h

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -56,6 +56,8 @@ class Square:
     def my_print(self):
         """_summary_"""
 
+        if self.__size == 0:
+            print()
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#")
